43df9bfa65a67b8cccdd1097cec61ef192bebdb0
jum1pking <129595116+jum1pking@users.noreply.github.com>
2023-04-25 21:21:21 +0800

    msg
    q
    ;
    q

---
+++ src/main/java/com/controller/HumanController.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@Controller
+@RequestMapping("/human")
+public class HuamnController {
+    @Resource
+
+
+
+}
